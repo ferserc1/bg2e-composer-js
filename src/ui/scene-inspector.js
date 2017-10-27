e644320c4708bd0b56ba58418f1b0d886201f88e
Fernando Serrano Carpena <ferserc1@gmail.com>
2017-10-27 20:24:02 +0200

    Scene tree view base implementation

--- src/ui/scene-inspector.js
+++ src/ui/scene-inspector.js
@@ -6,8 +6,121 @@ app.addDefinitions(() => {
 app.addSource(() => {
     let angularApp = angular.module(GLOBAL_APP_NAME);
 
-    angularApp.controller("SceneInspectorController", ['$scope',function($scope) {
+    angularApp.factory("RecursionHelper", ['$compile', function($compile) {
+        return {
+            compile: function(element, link) {
+                if (angular.isFunction(link)) {
+                    link = { post: link };
+                }
 
+                let contents = element.contents().remove();
+                let compiledContents;
+                return {
+                    pre: (link && link.pre) ? link.pre : null,
+
+                    post: function(scope,element) {
+                        if (!compiledContents) {
+                            compiledContents = $compile(contents);
+                        }
+                        compiledContents(scope,function(clone) {
+                            element.append(clone);
+                        });
+
+                        if (link && link.post) {
+                            link.post.apply(null, arguments);
+                        }
+                    }
+                };
+            }
+        }
+    }]);
+
+    angularApp.directive("tree", function(RecursionHelper) {
+        return {
+            restrict: "E",
+            scope: { family: '=' },
+            templateUrl: `templates/${ app.config.templateName }/directives/tree-item.html`,
+            compile: function(element) {
+                return RecursionHelper.compile(element);
+            },
+            controller: ['$scope',function($scope) {
+                $scope.toggleExpand = function() {
+                    $scope.family.expanded = !$scope.family.expanded;
+                }
+            }]
+        };
+    });
+
+    angularApp.controller("SceneInspectorController", ['$scope',function($scope) {
+        $scope.node = {
+            name: "Scene root",
+            expanded: true,
+            children: [
+                {
+                    name: "Cosa 1",
+                    expanded: true,
+                    children: [
+
+                    ]
+                },
+                {
+                    name: "Cosa 2",
+                    expanded: true,
+                    children: [
+                        
+                    ]
+                },
+                {
+                    name: "Cosa 3",
+                    expanded: true,
+                    children: [
+                        
+                    ]
+                },
+                {
+                    name: "Cosa 4",
+                    expanded: true,
+                    children: [
+                        
+                    ]
+                },
+                {
+                    name: "Cosa 5",
+                    expanded: true,
+                    children: [
+                        {
+                            name: "Cosa 5 1",
+                            expanded: true,
+                            children: [
+                                
+                            ]
+                        },
+                        {
+                            name: "Cosa 5 2",
+                            expanded: true,
+                            children: [
+                                
+                            ]
+                        },
+                        {
+                            name: "Cosa 5 3",
+                            expanded: true,
+                            children: [
+                                
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: "Cosa 6",
+                    expanded: true,
+                    children: [
+                        
+                    ]
+                }
+
+            ]
+        }
     }]);
 
     angularApp.directive('sceneInspector', () => {
