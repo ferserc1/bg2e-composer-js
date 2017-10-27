39be4cb96bc6b657ba084a94ae8ee48207f26eb2
Fernando Serrano Carpena <ferserc1@gmail.com>
2017-10-27 17:31:21 +0200

    Window state saved and restored

--- main.js
+++ main.js
@@ -3,19 +3,37 @@ const {app, BrowserWindow} = require('electron');
 const url = require('url');
 const path = require('path');
 const menu = require(__dirname + '/src-main/menu');
+const WindowStateManager = require('electron-window-state-manager');
 
 let win = null;
 
+const mainWindowState = new WindowStateManager('mainWindow', {
+    defaultWidth: 1024,
+    defaultHeight: 768
+});
+
 function createWindow() {
-    win = new BrowserWindow({width: 800, height: 600});
+    win = new BrowserWindow({
+        width: mainWindowState.width,
+        height: mainWindowState.height,
+        x: mainWindowState.x,
+        y: mainWindowState.y
+    });
+    if (mainWindowState.maximized) {
+        win.maximize();
+    }
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true
     }));
+    win.on('close', () => {
+        mainWindowState.saveState(win);
+    });
 }
 
 app.on('ready', () => {
     createWindow();
+
     menu.buildMenu();
 });
