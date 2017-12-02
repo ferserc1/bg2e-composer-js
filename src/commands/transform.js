03eb4dc2381dbd94fa6b849d2dfb4fc8dd030cfe
Fernando Serrano Carpena <ferserc1@gmail.com>
2017-12-02 20:03:36 +0100

    Transform component UI implemented.

--- src/commands/transform.js
+++ src/commands/transform.js
@@ -36,4 +36,126 @@ app.addSource(() => {
     }
 
     app.transformCommands.Transform = Transform;
+
+    function placeGizmo(node) {
+        let gizmo = node.component("bg.manipulation.Gizmo");
+        if (gizmo) {
+            let activeCamera = app.render.Scene.Get().camera;
+            gizmo.beginDrag(bg.manipulation.GizmoAction.NONE,new bg.Vector2(0,0));
+            gizmo.drag(bg.manipulation.GizmoAction.NONE,new bg.Vector2(0,0),new bg.Vector2(0,0),activeCamera);
+            gizmo.endDrag(bg.manipulation.GizmoAction.NONE);
+        }
+    }
+
+    class ResetPosition extends app.Command {
+        constructor(node) {
+            super();
+            this._node = node;
+            this._transform = node.transform;
+            this._prevMatrix = new bg.Matrix4(node.transform.matrix);
+        }
+
+        execute() {
+            return new Promise((resolve,reject) => {
+                this._transform.matrix.setPosition(0,0,0);
+                placeGizmo(this._node);
+                resolve();
+            })
+        }
+
+        undo() {
+            return new Promise((resolve,reject) => {
+                this._transform.matrix = new bg.Matrix4(this._prevMatrix);
+                placeGizmo(this._node);
+                resolve();
+            })
+        }
+    }
+
+    app.transformCommands.ResetPosition = ResetPosition;
+
+    class ResetRotation extends app.Command {
+        constructor(node) {
+            super();
+            this._node = node;
+            this._transform = node.transform;
+            this._prevMatrix = new bg.Matrix4(node.transform.matrix);
+        }
+
+        execute() {
+            return new Promise((resolve,reject) => {
+                let scale = this._transform.matrix.getScale();
+                this._transform.matrix.setRow(0,new bg.Vector4(1,0,0,0));
+                this._transform.matrix.setRow(1,new bg.Vector4(0,1,0,0));
+                this._transform.matrix.setRow(2,new bg.Vector4(0,0,1,0));
+                this._transform.matrix.scale(scale.x,scale.y,scale.z);
+                placeGizmo(this._node);
+                resolve();
+            })
+        }
+
+        undo() {
+            return new Promise((resolve,reject) => {
+                this._transform.matrix = new bg.Matrix4(this._prevMatrix);
+                placeGizmo(this._node);
+                resolve();
+            })
+        }
+    }
+
+    app.transformCommands.ResetRotation = ResetRotation;
+
+    class ResetScale extends app.Command {
+        constructor(node) {
+            super();
+            this._node = node;
+            this._transform = node.transform;
+            this._prevMatrix = new bg.Matrix4(node.transform.matrix);
+        }
+
+        execute() {
+            return new Promise((resolve,reject) => {
+                this._transform.matrix.setScale(1,1,1);
+                placeGizmo(this._node);
+                resolve();
+            })
+        }
+
+        undo() {
+            return new Promise((resolve,reject) => {
+                this._transform.matrix = new bg.Matrix4(this._prevMatrix);
+                placeGizmo(this._node);
+                resolve();
+            })
+        }
+    }
+
+    app.transformCommands.ResetScale = ResetScale;
+
+    class Reset extends app.Command {
+        constructor(node) {
+            super();
+            this._node = node;
+            this._transform = node.transform;
+            this._prevMatrix = new bg.Matrix4(node.transform.matrix);
+        }
+
+        execute() {
+            return new Promise((resolve,reject) => {
+                this._transform.matrix.identity();
+                placeGizmo(this._node);
+                resolve();
+            })
+        }
+
+        undo() {
+            return new Promise((resolve,reject) => {
+                this._transform.matrix = new bg.Matrix4(this._prevMatrix);
+                placeGizmo(this._node);
+                resolve();
+            })
+        }
+    }
+
+    app.transformCommands.Reset = Reset;
 })
---
+++ src/components/transform.js
@@ -0,0 +1,51 @@
+app.addSource(() => {
+    app.components.addComponent(() => {
+        return class TransformUI extends app.components.ComponentUI {
+            constructor() {
+                super("bg.scene.Transform","Transform","transform-ui");
+            }
+
+            resetPosition() {
+                app.CommandManager.Get().doCommand(
+                    new app.transformCommands.ResetPosition(this.componentInstance)
+                )
+                .then(() => app.ComposerWindowController.Get().updateView());
+            }
+
+            resetRotation() {
+                app.CommandManager.Get().doCommand(
+                    new app.transformCommands.ResetRotation(this.componentInstance)
+                )
+                .then(() => app.ComposerWindowController.Get().updateView());
+            }
+
+            resetScale() {
+                app.CommandManager.Get().doCommand(
+                    new app.transformCommands.ResetScale(this.componentInstance)
+                )
+                .then(() => app.ComposerWindowController.Get().updateView());
+            }
+
+            resetAll() {
+                app.CommandManager.Get().doCommand(
+                    new app.transformCommands.Reset(this.componentInstance)
+                )
+                .then(() => app.ComposerWindowController.Get().updateView());
+            }
+        }
+    });
+
+    let angularApp = angular.module(GLOBAL_APP_NAME);
+
+    angularApp.directive("transformUi", function() {
+        return {
+            restrict: "E",
+            templateUrl: `templates/${ app.config.templateName }/directives/transform-ui.html`,
+            scope: {
+                component: "="
+            },
+            controller: ["$scope",function($scope) {
+            }]
+        }
+    });
+})
