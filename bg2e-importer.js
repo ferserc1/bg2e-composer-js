f1c97073ee5baa1185d03128544110d0fe9bd645
Fernando Serrano Carpena <ferserc1@gmail.com>
2017-12-11 18:34:32 +0100

    Graphic engine import improved

---
+++ bg2e-importer.js
@@ -0,0 +1,35 @@
+(function() {
+    let fs = require("fs");
+    let path = require("path");
+
+    function importFile(file) {
+        let script = document.createElement("script");
+        script.src = file;
+        document.head.appendChild(script);
+    }
+
+    function importFolder(src) {
+        let sortFn = (a,b) => {
+            if (a<b) return -1;
+            else return 1;
+        };
+
+        let stat = fs.statSync(src);
+        if (stat.isDirectory()) {
+            let dirContents = fs.readdirSync(src);
+            dirContents.sort(sortFn);
+            dirContents.forEach((fileName) => importFolder(path.join(src,fileName)));
+        }
+        else {
+            importFile(src);
+        }
+    }
+
+    let enginePath = path.resolve(path.join(__dirname,"../bg2e-js/"));
+
+    let engineSource = path.join(enginePath,"src");
+    let importFileList = [];
+
+    importFolder(engineSource);
+
+})()
