472e55c86204e734064f56b8f9d11f12355af8ee
Fernando Serrano Carpena <ferserc1@gmail.com>
2017-12-20 17:09:23 +0100

    External plugin load implemented

--- window-main.js
+++ window-main.js
@@ -157,6 +157,101 @@ var BG2E_COMPOSER_DEBUG = !BG2E_COMPOSER_RELEASE;
             return settings.has(property);
         }
     };
+
+    function loadLog() {
+        // The log class is defined here, because we need to capture all the console messages
+        app.ui = app.ui || {};
+        
+        app.ui.LogLevel = {
+            INFO:"info",
+            WARNING:"warning",
+            ERROR:"error"
+        };
+    
+        let g_log = null;
+        class Log {
+            static Get() {
+                if (!g_log) {
+                    g_log = new Log();
+                }
+                return g_log;
+            }
+    
+            constructor() {
+                console.__log = console.log;
+                console.__warn = console.warn;
+                console.__error = console.error;
+                this._messages = [];
+                this._observers = {};
+                console.log = function(message) {
+                    Log.Get().log(message,app.ui.LogLevel.INFO);
+                };
+                console.warn = function(message) {
+                    Log.Get().log(message,app.ui.LogLevel.WARNING);
+                };
+                console.error = function(message) {
+                    Log.Get().log(message,app.ui.LogLevel.ERROR);
+                };
+            }
+    
+            get messages() {
+                return this._messages;
+            }
+    
+            get lastMessageData() {
+                let l = this._messages.length;
+                if (l) {
+                    return this._messages[l-1];
+                }
+                else {
+                    return {
+                        text:"",
+                        level:"info"
+                    }
+                }
+            }
+    
+            get lastMessage() { return this.lastMessageData.text; }
+            get lastLevel() { return this.lastMessageData.level; }
+    
+            log(message,level=app.ui.LogLevel.INFO) {
+                switch (level) {
+                case app.ui.LogLevel.INFO:
+                    if (console.__log) console.__log(message);
+                    break;
+                case app.ui.LogLevel.WARNING:
+                    if (console.__warn) console.__warn(message);
+                    break;
+                case app.ui.LogLevel.ERROR:
+                    if (console.__error) console.__error(message);
+                    break;
+                }
+                
+                this._messages.push({
+                    text:message,
+                    level:level
+                });
+                this.notifyLogChanged();
+            }
+    
+            logChanged(observer,callback) {
+                this._observers[observer] = callback;
+            }
+    
+            notifyLogChanged() {
+                for (let key in this._observers) {
+                    this._observers[key]();
+                }
+            }
+        }
+    
+        app.ui.Log = Log;
+    
+        // Initialize log
+        Log.Get();
+    }
+
+    loadLog();
     
     function loadApp() {
         let ng = angular.module(GLOBAL_APP_NAME, app.angular.deps);
@@ -184,11 +279,6 @@ var BG2E_COMPOSER_DEBUG = !BG2E_COMPOSER_RELEASE;
         }]);
 
         angular.bootstrap(document, [ GLOBAL_APP_NAME ]);
-
-        console.log("Plugin folders:");
-        app.plugins.paths.forEach((p) => {
-            console.log("  " + p);
-        })
     };
  
     if (BG2E_COMPOSER_DEBUG) {
@@ -196,6 +286,24 @@ var BG2E_COMPOSER_DEBUG = !BG2E_COMPOSER_RELEASE;
         requireSources(__dirname + '/src');
     }
     requireStylesheets();
+
+    console.log("Plugin folders:");
+    app.plugins.paths.forEach((p) => {
+        console.log("  " + p);
+    });
+    
+    app.plugins.paths.forEach((pluginPath) => {
+        if (fs.existsSync(pluginPath)) {
+            fs.readdirSync(pluginPath).forEach((plugin) => {
+                let fullPluginPath = path.join(pluginPath,plugin);
+                let stat = fs.statSync(fullPluginPath);
+                if (stat.isDirectory) {
+                    console.log("Plugin candidate found at " + fullPluginPath);
+                    requireSources(fullPluginPath);
+                }
+            })
+        }
+    });
 
     setTimeout(() => loadApp(), 100);
 
