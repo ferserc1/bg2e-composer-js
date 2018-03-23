f01d6c7056ba577a82f194d4680f3941bbf72e78
Fernando Serrano Carpena <ferserc1@gmail.com>
2018-03-23 11:28:15 +0100

    Library view UI: implemented list and navigation through groups

--- src/ui/library-inspector.js
+++ src/ui/library-inspector.js
@@ -2,18 +2,29 @@ app.addSource(() => {
     let angularApp = angular.module(GLOBAL_APP_NAME);
 
     angularApp.controller("LibraryInspectorController",['$scope',function($scope) {
+        let libMgr = app.library.Manager.Get();
         function update(library) {
             setTimeout(() => {
                 $scope.currentNode = library.currentNode;
+                $scope.navigator = library.navigator;
                 $scope.$apply();
             },50);
         }
 
-        app.library.Manager.Get().libraryChanged("libraryInspector",(library) => {
+        libMgr.libraryChanged("libraryInspector",(library) => {
             update(library);
         });
         
-        update(app.library.Manager.Get().current);
+        update(libMgr.current);
+
+        $scope.enterNode = function(node) {
+            libMgr.current.currentNode = node;
+            libMgr.notifyLibraryChanged();
+        };
+
+        $scope.selectNode = function(node) {
+            
+        };
     }]);
 
     angularApp.directive("libraryInspector", function() {
