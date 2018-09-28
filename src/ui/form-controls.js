dd6cf864515af52023d6e0018b73868941111271
Fernando Serrano Carpena <ferserc1@gmail.com>
2018-09-28 14:14:41 +0200

    Bug fixed in color picker

--- src/ui/form-controls.js
+++ src/ui/form-controls.js
@@ -56,7 +56,9 @@ app.addSource(() => {
                         + b + ',' +
                         + $scope.value[3] + ')';
                     
-                    $scope.color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
+                    $scope.color = "#" + (r<16 ? "0":"") + r.toString(16)
+                         + (g<16 ? "0":"") + g.toString(16)
+                         + (b<16 ? "0":"") + b.toString(16);
                 }, true);
 
                 
