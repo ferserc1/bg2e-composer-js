2d0840a4f0365d5f7439d157fcdc48e53c469940
Fernando Serrano Carpena <ferserc1@gmail.com>
2019-08-28 13:00:55 +0200

    Voxel update implemented (without commands)

--- src/components/voxel.js
+++ src/components/voxel.js
@@ -11,6 +11,19 @@ app.addSource(() => {
                 let comp = new bg.scene.Voxel();
                 return comp;
             }
+
+            updateComponentData(id,size,w,h,d,offset) {
+                // TODO: Implement using commands
+                try {
+                    this.componentInstance.sideSize = size;
+                    this.componentInstance.width = w;
+                    this.componentInstance.height = h;
+                    this.componentInstance.depth = d;
+                    this.componentInstance.offset = new bg.Vector3(offset);
+                } catch(err) {
+
+                }
+            }
         }
     });
 
@@ -36,30 +49,53 @@ app.addSource(() => {
         $scope.depth = $scope.sizeOptions[0];
         $scope.offset = [0,0,0];
 
-        $scope.setIdentifier = function() {
+        function findSideSize(id) {
+            let result = $scope.sizeOptions[0];
+            $scope.sizeOptions.some((s) => {
+                if (s.id==id) {
+                    result = s;
+                }
+                return result!=null;
+            });
+            return result;
+        }
 
+        function updateComponent() {
+            $scope.component.updateComponentData(
+                $scope.voxelId,
+                $scope.sideSize,
+                $scope.width.id,
+                $scope.height.id,
+                $scope.depth.id,
+                $scope.offset
+            );
+            app.ComposerWindowController.Get().updateView();
+        }
+
+        $scope.setSideSize = function(v) { updateComponent(); };
+        $scope.setWidth = function(v) { updateComponent(); };
+        $scope.setHeight = function(v) { updateComponent(); };
+        $scope.setDepth = function(v) { updateComponent(); };
+        $scope.setOffset = function(v) { updateComponent(); };
+
+        $scope.updateValues = function() {
+            let comp = $scope.component.componentInstance;
+            $scope.voxelId = comp.identifier;
+            $scope.sideSize = comp.sideSize;
+            $scope.width = findSideSize(comp.width);
+            $scope.height = findSideSize(comp.height);
+            $scope.depth = findSideSize(comp.depth);
+            $scope.offset = comp.offset.toArray();
         };
 
-        $scope.setSideSize = function() {
+        app.render.Scene.Get().selectionManager.selectionChanged("voxelUi", () => {
+            setTimeout(() => {
+                $scope.updateValues();
+                $scope.$apply();
+            });
+        });
 
-        };
-
-        $scope.setWidth = function() {
-
-        };
-
-        $scope.setHeight = function() {
-
-        };
-
-        $scope.setDepth = function() {
-
-        };
-
-        $scope.setOffset = function() {
-
-        };
-
+        $scope.updateValues();
     }]);
 
     angularApp.directive("voxelUi", function() {
